b3efea7a0205145fdf2e00faf82527ecdf3ec484
Ryan Linton <ryanlntn@gmail.com>
2014-05-10 22:46:08 -0700

    Draw sprite

--- app/layers/grid_layer.rb
+++ app/layers/grid_layer.rb
@@ -2,6 +2,9 @@ class GridLayer < Joybox::Core::Layer
   scene
 
   def on_enter
-    puts "Entering GridLayer..."
+    SpriteFrameCache.frames.add file_name: "sprites.plist"
+    @sprite_batch = SpriteBatch.new file_name: "sprites.png"
+    @sprite_batch << Sprite.new(frame_name: 'boy.png', position: [Screen.half_width, Screen.half_height])
+    self << @sprite_batch
   end
 end
