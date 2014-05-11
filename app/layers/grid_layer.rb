a1312a578a28b744bb1a0ea6286b21bf50302473
Ryan Linton <ryanlntn@gmail.com>
2014-05-10 22:47:28 -0700

    Draw more sprites

--- app/layers/grid_layer.rb
+++ app/layers/grid_layer.rb
@@ -5,6 +5,8 @@ class GridLayer < Joybox::Core::Layer
     SpriteFrameCache.frames.add file_name: "sprites.plist"
     @sprite_batch = SpriteBatch.new file_name: "sprites.png"
     @sprite_batch << Sprite.new(frame_name: 'boy.png', position: [Screen.half_width, Screen.half_height])
+    @sprite_batch << Sprite.new(frame_name: 'girl_cat.png', position: [0, 0])
+    @sprite_batch << Sprite.new(frame_name: 'girl_cat.png', position: [Screen.width - 20, Screen.height - 20])
     self << @sprite_batch
   end
 end
