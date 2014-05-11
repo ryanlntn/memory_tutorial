571617b9b44e4c944792d803fa37a4211ae4c372
Ryan Linton <ryanlntn@gmail.com>
2014-05-10 23:04:34 -0700

    Draw grid

--- app/layers/grid_layer.rb
+++ app/layers/grid_layer.rb
@@ -2,11 +2,26 @@ class GridLayer < Joybox::Core::Layer
   scene
 
   def on_enter
+    load_sprite_sheet
+    load_tiles
+  end
+
+  def load_tiles
+    @tiles = 4.times.map do |row|
+      4.times.map do |column|
+        Sprite.new frame_name: 'hidden.png', position: [
+          column * 64 + 64,
+          row * 64 + 128
+        ]
+      end
+    end.flatten
+
+    @tiles.each { |t| @sprite_batch << t }
+  end
+
+  def load_sprite_sheet
     SpriteFrameCache.frames.add file_name: "sprites.plist"
     @sprite_batch = SpriteBatch.new file_name: "sprites.png"
-    @sprite_batch << Sprite.new(frame_name: 'boy.png', position: [Screen.half_width, Screen.half_height])
-    @sprite_batch << Sprite.new(frame_name: 'girl_cat.png', position: [0, 0])
-    @sprite_batch << Sprite.new(frame_name: 'girl_cat.png', position: [Screen.width - 20, Screen.height - 20])
     self << @sprite_batch
   end
 end
